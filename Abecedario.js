12f173de99a7f4d3d8f9b08df9aa8490b6668314
Eduardo Duarte de Sant'Anna <duartedesantanna@gmail.com>
2021-06-09 14:06:21 -0300

    Programa que simula o jogo ABC(Stop).

---
+++ Abecedario.js
@@ -0,0 +1,9 @@
+function solucao(letra, palavras) {
+    let erros = 0;
+	for(let item of palavras){
+        if(item[0] !== letra){
+            erros++;
+        }
+    }
+    console.log(erros);
+}
